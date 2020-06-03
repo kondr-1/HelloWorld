03fa8fe49ff683d2ec77ab4652d70e43e951ce0d
Anton Kondralev <motovolg-1@yandex.ru>
2020-06-03 11:05:53 +0400

    hello world

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        String Hello = "Hello";
+        String World = "World";
+        System.out.println(Hello +" "+ World +"!");
+
+//
+
+//        int a = 1;
+//        int b = 2;
+//        int c = a+b;
+//        System.out.println("sum =" +c);
+
+//        int i = 1;
+//        System.out.println(i);
+//        i = i+1;
+//        System.out.println(i);
+//        i = i+1;
+//        System.out.println(i);
+//        System.out.println("1.");
+//        System.out.println("2..");
+//        System.out.println("3...");
+        System.out.println("Hello World!");
+    }
+}
